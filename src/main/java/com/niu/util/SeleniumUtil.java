9d8d50eae61b98c978891cfaf2201368206b2ca8
hff <2506694095@qq.com>
2023-09-19 20:22:29 +0800

    bug fix

--- src/main/java/com/niu/util/SeleniumUtil.java
+++ src/main/java/com/niu/util/SeleniumUtil.java
@@ -38,7 +38,7 @@ import java.util.Set;
 public class SeleniumUtil {
     private static EdgeDriver driver;
 
-    private static final String TENCENT_URL_SEC = "https://api.kit9.cn/api/360_security/api.php?url=";
+    private static final String TENCENT_URL_SEC = "https://api.kit9.cn/api/tencent_security/api.php?url=";
 
     @Autowired
     public BotConfig botConfig;
@@ -59,7 +59,7 @@ public class SeleniumUtil {
     }
 
     public static InputStream screenshot(String message) throws InterruptedException, IOException {
-        if (urlsec(message)){
+        if (!urlsec(message)){
             return null;
         }
         //获取要截图的地址，注：需要先获取地址哦，不然下方获取的宽度高度就会是弹窗的高和宽，而不是页面内容的高宽
